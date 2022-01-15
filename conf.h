7822545d03dea2588c4d8966098cd996fa7d1668
Maverick <gjs990825@gmail.com>
2022-01-15 22:46:26 +0800

    Update server ip adress.

--- conf.h
+++ conf.h
@@ -6,7 +6,7 @@
 // #define PREFERENCES_DEBUG
 
 // BT
-static const char *const blueToothName = "IOT_Socket";
+static const char *const blueToothName = "IoT_Socket";
 
 // WIFI
 static String ssid = "";
@@ -15,8 +15,8 @@ static String password = "";
 // Server
 // static const IPAddress serverIPAdress = IPAddress(192, 168, 123, 100);
 // static const String serverIPAdressString = "192.168.123.100";
-static const IPAddress serverIPAdress = IPAddress(121, 40, 62, 163);
-static const String serverIPAdressString = "121.40.62.163";
+static const IPAddress serverIPAdress = IPAddress(120, 24, 84, 40);
+static const String serverIPAdressString = "120.24.84.40";
 static const int serverPort = 8080;
 static const String serverBaseAddress = "http://" + serverIPAdressString + ":" + serverPort + "/IOT_Socket";
 static const String serverDataHandler = serverBaseAddress + "/ESP32UploadData";
